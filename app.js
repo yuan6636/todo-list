328cd1fbe8b46b98ed726c974ccfc6f3467c0310
yuan6636 <aleniverson777@gmail.com>
2024-05-15 16:29:13 +0800

    router init

--- app.js
+++ app.js
@@ -1,10 +1,39 @@
 const express = require('express')
 const app = express()
+const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('hello world!')
+    res.send('hello world!')
 })
 
-app.listen(3000, () => {
-  console.log('App is running on port 3000')
+app.get('/todos', (req, res) => {
+    res.send('get all todos')
+})
+
+app.get('/todos/new', (req, res) => {
+    res.send('get todo form')
+})
+
+app.post('/todos', (req, res) => {
+    res.send('add todo')
+})
+
+app.get('/todos/:id', (req, res) => {
+    res.send(`get todo: ${req.params.id}`)
+})
+
+app.get('/todos/:id/edit', (req, res) => {
+    res.send(`get todo edit form, id: ${req.params.id}`)
+})
+
+app.put('/todos/:id', (req, res) => {
+    res.send(`todo id: ${req.params.id} has been modified`)
+})
+
+app.delete('/todos/:id', (req, res) => {
+    res.send(`todo id: ${req.params.id} has been deleted`)
+})
+
+app.listen(port, () => {
+  console.log('App is running on http://localhost:3000')
 })
