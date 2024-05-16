d0b9a65664191cc3d55e92a550ffe1aab2372aee
yuan6636 <aleniverson777@gmail.com>
2024-05-16 11:24:08 +0800

    implement creating todo

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.engine('.hbs', engine({ extname: '.hbs' }))
 app.set('view engine', '.hbs')
 app.set('views', './views')
 
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
     res.render('index')
 })
@@ -25,11 +27,13 @@ app.get('/todos', (req, res) => {
 })
 
 app.get('/todos/new', (req, res) => {
-    res.send('get todo form')
+    return res.render('new')
 })
 
 app.post('/todos', (req, res) => {
-    res.send('add todo')
+    const name = req.body.name
+    return Todo.create({ name })
+        .then(() => res.redirect('/todos'))
 })
 
 app.get('/todos/:id', (req, res) => {
