92c2585182f4ecfc0d46b8263e504f8e003fb3e6
yuan6636 <aleniverson777@gmail.com>
2024-05-17 16:15:49 +0800

    use database pagination

--- routes/todos.js
+++ routes/todos.js
@@ -10,10 +10,12 @@ router.get('/', (req, res, next) => {
 
        return Todo.findAll({
               attributes: ['id', 'name', 'isComplete'],
+              offset: (page -1) * limit,
+              limit,
               raw: true
        })
             .then((todos) => res.render('todos', { 
-                  todos: todos.slice((page-1) * limit, page * limit),
+                  todos,
                   prev: page > 1 ? page - 1 : page,
                   next: page + 1,
                   page
