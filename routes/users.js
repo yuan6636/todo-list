1727ea8f22838762cfffda281d8d6de9f15e4582
yuan6636 <aleniverson777@gmail.com>
2024-05-27 15:24:33 +0800

    implement register

--- routes/users.js
+++ routes/users.js
@@ -4,8 +4,40 @@ const router = express.Router()
 const db = require('../models')
 const User = db.User
 
-router.post('/', (req, res) => {
-    return res.send(req.body)
+router.post('/', (req, res, next) => {
+    const { email, name, password, confirmPassword } = req.body
+
+    if (!email || !password) {
+        req.flash('error', 'email 及 password 為必填')
+        return res.redirect('back')
+    }
+
+    if (password !== confirmPassword) {
+        req.flash('error', '驗證密碼與密碼不符')
+        return res.redirect('back')
+    }
+
+    return User.count({ where: { email } })
+        .then((rowCount) => {
+            if (rowCount > 0 ) {
+                req.flash('error', 'email已註冊')
+                return 
+            }
+
+            return User.create({ email, name, password})
+        })
+        .then((user) => {
+            if (!user) {
+                return res.redirect('back')
+            }
+
+            req.flash('success', '註冊成功')
+            return res.redirect('/login')
+        })
+        .catch((error) => {
+            error.errorMessage = '註冊失敗'
+            next(error)
+        })
 })
 
 module.exports = router
