02d26d0485610b9082321ef33f83b5b2fd3cb163
yuan6636 <aleniverson777@gmail.com>
2024-08-28 12:04:17 +0800

    support facebook OAuth2.0 login/register

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcryptjs')
 
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
+const FacebookStrategy = require('passport-facebook')
 
 const db = require('../models')
 const User = db.User
@@ -31,6 +32,35 @@ passport.use(new LocalStrategy({ usernameField: 'email' }, (username, password, 
             .catch((error) => {
                 error.errorMessage = '登入失敗'
                 done(error)
+            })
+}))
+
+passport.use(new FacebookStrategy({
+        clientID: process.env.FACEBOOK_CLIENT_ID,
+        clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+        callbackURL: process.env.FACEBOOK_CALLBACK_URL,
+        profileFields: ['email', 'displayName']
+}, (accessToken, refreshToken, profile, done) => {
+      const email = profile.emails[0].value
+      const name = profile.displayName
+
+      return User.findOne({
+              attributes: ['id', 'name', 'email'],
+              where: { email },
+              raw: true
+      })
+            .then((user) => {
+                  if (user) return done(null, user)
+                  
+                  const randomPwd = Math.random().toString(36).slice(-8)
+
+                  return bcrypt.hash(randomPwd, 10)
+                          .then((hash) => User.create({name, email, password: hash}))
+                          .then((user) => done(null, { id: user.id, name: user.name, email: user.email}))
+            })
+            .catch((error) => {
+                  error.errorMessage = '登入失敗'
+                  done(error)
             })
 }))
 
@@ -66,6 +96,14 @@ router.post('/login', passport.authenticate('local', {
     successRedirect: '/todos',
     failureRedirect: '/login',
     failureFlash: true
+}))
+
+router.get('/login/facebook', passport.authenticate('facebook', { scope: ['email'] }))
+
+router.get('/oauth2/redirect/facebook', passport.authenticate('facebook', {
+        successRedirect: '/todos',
+        failureRedirect: '/login',
+        failureFlash: true
 }))
 
 router.post('/logout', (req, res) => {
